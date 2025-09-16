e3b8bc506fd41a0c8fb54944a48a87b719cebdc0
AjTheSpidey <168258894+AjTheSpidey@users.noreply.github.com>
2025-09-16 19:55:20 +0800

    Create instr.py

---
+++ instr.py
@@ -0,0 +1,32 @@
+txt_title = 'Health'
+win_x, win_y = 200, 100
+win_width, win_height = 1000, 600
+
+
+txt_hello = 'dummy'
+txt_next = 'y'
+txt_instruction = "This application allows you to use the Rufier test to make an initial diagnosis of your health.
+The Rufier test is a set of physical exercises designed to assess your cardiac performance during physical exertion. The subject lies in the supine position for 5 minutes and has their pulse rate measured for 15 seconds;
+then, within 45 seconds, the subject performs 30 squats.
+When the exercise ends, the subject lies down and their pulse is measured again for the first 15 seconds
+and then for the last 15 seconds of the first minute of the recovery period.
+Important! If you feel unwell during the test (dizziness,
+tinnitus, shortness of breath, etc.), stop the test and consult a physician." 
+txt_name = 'y'
+txt_hintname = "y"
+txt_hintage = "y"
+txt_test1 = 'y'
+txt_test2 = 'y'
+txt_test3 = 'y'
+txt_sendresults = 'y'
+txt_hinttest1 = '0'
+txt_hinttest2 = '0'
+txt_hinttest3 = '0'
+txt_starttest1 = 'y'
+txt_starttest2 = 'y'
+txt_starttest3 = 'y'
+
+txt_age = 'y'
+txt_finalwin = 'y'
+txt_index = 'y'
+txt_workheart = 'y'
