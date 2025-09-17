b82c06fca108822d4a98ad98ef185d2232993ab8
Phoenix Gutierrez <sofia.nicole.c.gutierrez>
2025-09-17 18:38:53 +0800

    main

--- final_win.py
+++ final_win.py
@@ -10,6 +10,13 @@ class TestWin(QWidget):
         self.connects()
         self.show()
     def set_appear(self):
-        self.setWindowTitle(txt_title)
+        self.setWindowTitle("Results")
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
+    def initUI(self):
+        self.layout = QVBoxLayout()
+
+        self.txt_name = QLabel(txt_name)
+        self.l_line.addWidget(self.txt_name, alignment = Qt.AlignLeft)
+
+        self.setLayout(self.layout)
