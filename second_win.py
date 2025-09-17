164c35ef2bd05c7358de98af75f3075b28b6c1c0
Phoenix Gutierrez <sofia.nicole.c.gutierrez>
2025-09-17 18:09:02 +0800

    main

--- second_win.py
+++ second_win.py
@@ -17,11 +17,52 @@ class TestWin(QWidget):
         self.h_line = QHBoxLayout()
         self.r_line = QVBoxLayout()
         self.l_line = QVBoxLayout()
+
+        self.txt_name = QLabel(txt_name)
         self.l_line.addWidget(self.txt_name, alignment = Qt.AlignLeft)
 
         self.name = QLineEdit(txt_hintname)
-
         self.l_line.addWidget(self.name, alignment = Qt.AlignLeft)
+
+        self.txt_hintage = QLabel(txt_hintage)
+        self.l_line.addWidget(self.txt_hintage, alignment = Qt.AlignLeft)
+
+        self.age = QLineEdit("0")
+        self.l_line.addWidget(self.age, alignment = Qt.AlignLeft)
+
+        self.test1 = QLabel(txt_test1)
+        self.l_line.addWidget(self.test1, alignment = Qt.AlignLeft)
+
+        self.starttest1 = QPushButton(txt_starttest1)
+        self.l_line.addWidget(self.starttest1, alignment = Qt.AlignLeft)
+
+        self.hinttest1 = QLineEdit("0")
+        self.l_line.addWidget(self.hinttest1, alignment = Qt.AlignLeft)
+
+        self.test2 = QLabel(txt_test2)
+        self.l_line.addWidget(self.test2, alignment = Qt.AlignLeft)
+
+        self.starttest2 = QPushButton(txt_starttest2)
+        self.l_line.addWidget(self.starttest2, alignment = Qt.AlignLeft)
+
+        self.test3 = QLabel(txt_test3)
+        self.l_line.addWidget(self.test3, alignment = Qt.AlignLeft)
+
+        self.starttest3 = QPushButton(txt_starttest3)
+        self.l_line.addWidget(self.starttest3, alignment = Qt.AlignLeft)
+
+        self.hinttest2 = QLineEdit("0")
+        self.l_line.addWidget(self.hinttest2, alignment = Qt.AlignLeft)
+
+        self.hinttest2 = QLineEdit("0")
+        self.l_line.addWidget(self.hinttest2, alignment = Qt.AlignLeft)
+
+        self.sendresults = QPushButton(txt_sendresults)
+        self.l_line.addWidget(self.sendresults, alignment = Qt.AlignCenter)
+
+        self.timer = QLabel("00:00:09")
+        self.r_line.addWidget(self.timer, alignment = Qt.AlignRight)
+
         self.h_line.addLayout(self.l_line)
         self.h_line.addLayout(self.r_line)
         self.setLayout(self.h_line)
